dc9259df06839127ef0929beafaf6e2e1c1b063a
randombrunch <17028@my.sanctamaria.school.nz>
2022-02-14 12:50:48 +1300

    Created Component For Pizza Bot
    Welcome Component

---
+++ Components/welcome.py
@@ -0,0 +1,13 @@
+import random
+from random import randint
+
+# List of random names
+names = ["Mark", "Sally", "Robbie", "Phill", "Fredrick", "Jeremy", "Gregory", "Susan", "Fritz", "Gabriel"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print("*** Welcome to The Best Pizza You Will Ever Eat ***")
+print("*** My name is" ,name, "***")
+print("You and me we got this Im here to hep you order your BEST pizza")

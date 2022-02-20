80b733e33e6c6a8266f7bb09519fb9c15814c742
randombrunch <17028@my.sanctamaria.school.nz>
2022-02-21 10:20:47 +1300

    Created version 1 of pickup function
    Basic instructions and input for name and phone number

---
+++ Components/pickup v1.py
@@ -0,0 +1,13 @@
+# Bug - Accepts blank input
+
+print("Alright now I need you to fill out the pickup information")
+
+# Customer name
+name = input("Alright first tell me your name")
+
+
+# Customer phone number     
+phone = input("Now can you please enter your phone number")
+
+print (name)
+print (phone)

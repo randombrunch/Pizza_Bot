6f036fb99fb7f0adc95b81bfdec126ad8c9ef0b1
randombrunch <17028@my.sanctamaria.school.nz>
2022-05-13 01:49:55 +1200

    Added order print
    Create python for order print component

--- Components/order_print_v1.py
+++ Components/order_print_v1.py
@@ -4,6 +4,14 @@ order_list = ['Pepperoni', 'Margherita', 'Hawaiian', 'BBQ Chicken Deluxe']
 order_cost = [8.50, 8.50, 8.50, 13.50]
 
 
+cust_details = {'name': 'Mark', 'phone': '274609365', 'house': '45', 'street': 'Harry', 'suburb': 'Howick'}
+
+
+print("\n Name: {} \n Phone: {} \n House: {} \n Street: {} \n Suburb: {}" .format(cust_details['name'], cust_details['phone'], cust_details['house'], cust_details['street'], cust_details['suburb'] ))
+
+
+
+
 count = 0
 for item in order_list:
     print("Ordered: {}  Cost ${:.2f}".format(item, order_cost[count]))

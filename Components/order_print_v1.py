a9e8e19dd8f5af0fc1b6fccdf944c6b3623e2a78
randombrunch <17028@my.sanctamaria.school.nz>
2022-05-13 02:04:56 +1200

    Print order function
    Added formatting to print order and added total cost including function

--- Components/order_print_v1.py
+++ Components/order_print_v1.py
@@ -7,12 +7,18 @@ order_cost = [8.50, 8.50, 8.50, 13.50]
 cust_details = {'name': 'Mark', 'phone': '274609365', 'house': '45', 'street': 'Harry', 'suburb': 'Howick'}
 
 
-print("\n Name: {} \n Phone: {} \n House: {} \n Street: {} \n Suburb: {}" .format(cust_details['name'], cust_details['phone'], cust_details['house'], cust_details['street'], cust_details['suburb'] ))
+def print_order():
+    total_cost = sum(order_cost)
+    print ("Customer Details")
+    print (f"Customer Name: {cust_details['name']} \nCustomer Phone: {cust_details['phone']} \nCustomer Address: {cust_details['house']} {cust_details['street']} {cust_details['suburb']}")
+    print() 
+    print("Order Details")
+    count = 0
+    for item in order_list:
+        print("Ordered: {}  Cost ${:.2f}".format(item, order_cost[count]))
+        count = count+1
+    print()
+    print("Total Order Cost")
+    print(f"${total_cost:.2f}")
 
-
-
-
-count = 0
-for item in order_list:
-    print("Ordered: {}  Cost ${:.2f}".format(item, order_cost[count]))
-    count = count+1
+print_order()
